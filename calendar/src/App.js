8aff006ef0e185644eb13d240573a0431c60dd0f
vaimikpatel2908 <Vaimikpatel2908@gmail.com>
2019-05-01 17:40:57 -0400

    react calendar demo

---
+++ calendar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calendar from './Component/Calendar/';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
